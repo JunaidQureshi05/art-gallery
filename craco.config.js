b2541b773a6c2850fe780355ad1bb75d539a8a78
Junaid Qureshi <Junaid.562000@gmail.com>
2025-03-12 10:17:27 +0530

    override webpack configs using craco

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@scss": path.resolve(__dirname, "src/scss"), // ✅ Create alias for SCSS files
+    },
+  },
+};
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,17 @@
 import React from "react";
 import Switch from "../Switch";
-import "./Header.css";
+import s from "./Header.module.scss";
 import { signInWithGoogle } from "../../firebase/firebaseAuth";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "../../store/reducers/authReducer";
 import UserProfile from "../UserProfile";
 
 const Header = () => {
-  const dispatch = useDispatch();
-  const userProfile = useSelector((state) => state?.auth?.user);
-  console.log("user", userProfile);
   return (
     <nav>
       <h1>Art Loom</h1>
-      <div className="buttons">
-        <ul className="navigation">
+      <div className={s.buttons}>
+        <ul className={s.navigation}>
           <li>
             <a href="#"> Home</a>
           </li>
