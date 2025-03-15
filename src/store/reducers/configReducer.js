bc68cab885054e76f33ce080c4026491fcedc11f
Junaid Qureshi <Junaid.562000@gmail.com>
2025-03-15 10:26:38 +0530

    bugfix

--- src/store/reducers/configReducer.js
+++ src/store/reducers/configReducer.js
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { changeThemeAction } from "store/actions/config.actions";
 
 let pastTheme = window.localStorage.getItem("theme");
+document.body.setAttribute("data-theme", pastTheme || "light");
 
 const configSlice = createSlice({
   name: "configurations",
