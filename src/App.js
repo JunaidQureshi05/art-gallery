208d3760f3c8c5d129750525889ae31be7783e4b
Junaid Qureshi <Junaid.562000@gmail.com>
2025-03-15 11:13:16 +0530

    fix module import issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import ProductPage from "pages/Product";
+import ProductPage from "./pages/Product";
 import Header from "@components/Header";
 const App = () => {
   return (
