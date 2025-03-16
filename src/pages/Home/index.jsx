05b78b28edcef02aae3d22463c55e8e3f53f2653
Junaid Qureshi <Junaid.562000@gmail.com>
2025-03-16 12:06:16 +0530

    .

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,7 +6,12 @@ const Home = () => {
   return (
     <div>
       <div className={s.videoSection}>
-        <video src="BG_VIDEO.mp4" autoPlay muted loop />
+        <video
+          src="https://njjspvufrktahgpkmukd.supabase.co/storage/v1/object/sign/videos/BG_VIDEO.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJ2aWRlb3MvQkdfVklERU8ubXA0IiwiaWF0IjoxNzQyMTA2NDkwLCJleHAiOjQ4OTU3MDY0OTB9.V6HGhXQY5DLFPikGCIAp3Nl8PWTmPkbJ8ugkpSrofCc"
+          autoPlay
+          muted
+          loop
+        />
         <div className={s.quoteContainer}>
           <blockquote>
             “Painting is just another way of keeping a diary.”
