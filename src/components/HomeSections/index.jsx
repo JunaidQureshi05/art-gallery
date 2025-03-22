154f3b97c5afbd8e82852e64aca91e6ce59addfb
Junaid Qureshi <Junaid.562000@gmail.com>
2025-03-22 12:14:44 +0530

    fix home page design

--- src/components/HomeSections/index.jsx
+++ src/components/HomeSections/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./HomeSections.css";
+import s from "./HomeSections.module.scss";
 import PaintingCard from "../PaintingCard";
 import DUMMY_PAINTINGS from "../../data";
 
@@ -14,18 +14,18 @@ const CATEGORIES = [
 
 const HomeSections = () => {
   return (
-    <div className="home-sections">
+    <div className={s.homeSections}>
       {CATEGORIES.map((category, idx) => {
         return (
-          <div className="section" key={idx}>
-            <div className="section-header">
-              <h1 className="sectionTitle">{category}</h1>
-              <Link to={`/category/${category}`} className="viewMore">
+          <div className={s.section} key={idx}>
+            <div className={s.sectionHeader}>
+              <h1 className={s.sectionTitle}>{category}</h1>
+              <Link to={`/category/${category}`} className={s.viewMore}>
                 View More →
               </Link>
             </div>
 
-            <div className="section-items">
+            <div className={s.sectionItems}>
               {DUMMY_PAINTINGS.slice(0, 5).map((painting) => (
                 <PaintingCard painting={painting} key={painting.id} />
               ))}
